2d2f9649d3ebbf96b1553015a2efb1882c9c3e06
Kyle Finley <finpup@gmail.com>
2017-11-09 23:01:39 -0500

    Further updates, but stuck. Need to check out other project.

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import Component from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Index extends Component {
+  render() {
+    return (
+      <div>this is a test</div>
+    )
+  }
+}
+
+ReactDOM.render(Index, document.getElementById(root));
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,36 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'inline-source-map',
-  extensions: ['.js', '.jsx'],
-  plugins: {
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: "YLC 2018"
+      title: 'YLC 2018',
+      filename: 'static/index.html'
     })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+      }
+    ]
   },
-  modules: {
-    [
-      path.resolve(__dirname, "src"),
-      path.resolve(__dirname, "assets"),
-      path.resolve(__dirname, "styles"),
-      "node_modules"
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
+    modules: [
+        path.resolve(__dirname, "src"),
+        path.resolve(__dirname, "assets"),
+        path.resolve(__dirname, "styles"),
+        "node_modules"
     ]
   }
 };
