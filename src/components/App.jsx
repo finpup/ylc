fcc7ca4f395e7fd1483b8944383d2fd13542c96d
Kyle Finley <finpup@gmail.com>
2017-11-11 13:33:48 -0500

    Further updates to get the basic app compiling.

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'styles.scss';
+
+export default class App extends Component {
+	render() {
+		return(
+			<div>this is the YLC app</div>
+		)
+	}
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import Component from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App.jsx'
 
