5a3a9e100e7c9b9f94c1ec390de5f241874cdb65
Kyle Finley <finpup@gmail.com>
2017-11-18 15:48:15 -0500

    Started the speaker section.

--- src/content/speaker/Speaker.jsx
+++ src/content/speaker/Speaker.jsx
@@ -4,7 +4,26 @@ import '_speaker.scss';
 export default class Speaker extends Component {
 	render() {
 		return(
-			<div>speaker</div>
+			<section id="speaker">
+				<div className="topic">
+					<div className="topic__label">
+						<div className="topic__label__text">topic</div>
+					</div>
+					<div className="topic__name">
+						<div className="topic__name__text">Do you love me?</div>
+						<div className="topic__name__scripture">John 21</div>
+					</div>
+				</div>
+				<div className="speaker">
+					<div className="speaker__img">
+						<div className="speaker__img__stripes"></div>
+						<img src="../assets/a_quigley.jpg" alt="Andrew Quigley profile" />
+					</div>
+					<div className="speaker__profile">
+						
+					</div>
+				</div>
+			</section>
 		)
 	}
 }
