18c6b678ac213585c651a5bee778142e0bdf4ccf
Kyle Finley <finpup@gmail.com>
2017-11-18 15:10:55 -0500

    Added mission section.

--- src/content/mission/Mission.jsx
+++ src/content/mission/Mission.jsx
@@ -4,7 +4,19 @@ import '_mission.scss';
 export default class Mission extends Component {
 	render() {
 		return(
-			<div>mission</div>
+			<section id="mission">
+				<div id="whatIsYLC">
+					<div className="whatIsYLC_header">
+						<div className="whatIsYLC_header__text">What is YLC?</div>
+					</div>
+					<div className="whatIsYLC_body">
+						<div className="whatIsYLC_body__text">What is Christian leadership?</div>
+						<div className="whatIsYLC_body__text">How can I serve the church as I transition from being a teenager to an adult?</div>
+						<div className="whatIsYLC_body__text">How does my relationship with Christ affect my life?</div>
+						<div className="whatIsYLC_body__text">The goal of YLC is to provide a unique opportunity for you, Christian young adults, to hear and respond to a serious call to a life of service to Jesus Christ and grow in leadership.</div>
+					</div>
+				</div>
+			</section>
 		)
 	}
 }
