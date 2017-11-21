ed72067bf9a3356499eea7688a0828c8506dcd3f
Kyle Finley <finpup@gmail.com>
2017-11-20 23:30:49 -0500

    Finished the rough initial code of the speaker section.

--- src/content/speaker/Speaker.jsx
+++ src/content/speaker/Speaker.jsx
@@ -15,12 +15,20 @@ export default class Speaker extends Component {
 					</div>
 				</div>
 				<div className="speaker">
-					<div className="speaker__img">
-						<div className="speaker__img__stripes"></div>
-						<img src="../assets/a_quigley.jpg" alt="Andrew Quigley profile" />
-					</div>
-					<div className="speaker__profile">
-						
+					<div className="speaker-wrapper">
+						<div className="speaker__img">
+							<div className="speaker__img__stripes"></div>
+							<img src="../assets/a_quigley.jpg" alt="Andrew Quigley profile" />
+						</div>
+						<div className="speaker-right">
+							<div className="speaker__header">
+								<div className="speaker__header__label">speaker</div>
+								<div className="speaker__header__text">Andrew Quigley</div>
+							</div>
+							<div className="speaker__profile">
+								<div className="speaker__profile__text">Rev. Andrew Quigley has been a minister in  the Airdrie RP church in Scotland since 1994. He has spoken at a number of RPCNA Family Conferences and Youth Retreats.</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</section>
