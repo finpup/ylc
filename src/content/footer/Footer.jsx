936eef998935b3ac7b5ea512e5a86dcbeac46a39
Kyle Finley <finpup@gmail.com>
2018-01-13 18:11:29 -0500

    Added a email contact to run at the bottom of the footer.

--- src/content/footer/Footer.jsx
+++ src/content/footer/Footer.jsx
@@ -5,21 +5,26 @@ export default class Footer extends Component {
 	render() {
 		return(
 			<div id="footer">
-        <div id="footer__info">
-          <div id="footer__info__img">
-            <img src="../assets/footerLogo.png" alt="" />
-          </div>
-          <div id="footer__info__title">youth leadership conference 2018</div>
-        </div>
-        <div id="footer__registration">
-          <div className="register">
-            <a href="https://rpmissions.wufoo.com/forms/z1fawgp81q9rvvn/" target="_blank">
-              <div className="register__btn">
-                <div className="register__btn__text">register</div>
-              </div>
-            </a>
-          </div>
-        </div>
+				<div id="footer__wrapper">
+					<div id="footer__wrapper__info">
+	          <div id="footer__wrapper__info__img">
+	            <img src="../assets/footerLogo.png" alt="" />
+	          </div>
+	          <div id="footer__wrapper__info__title">youth leadership conference 2018</div>
+	        </div>
+	        <div id="footer__wrapper__registration">
+	          <div className="register">
+	            <a href="https://rpmissions.wufoo.com/forms/z1fawgp81q9rvvn/" target="_blank">
+	              <div className="register__btn">
+	                <div className="register__btn__text">register</div>
+	              </div>
+	            </a>
+	          </div>
+	        </div>
+				</div>
+				<div id="footer__contact">
+					<div id="footer__contact__text">Questions?&nbsp;<a href="mailto:&#121;&#109;&#099;&#115;&#121;&#110;&#111;&#100;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;?subject=YLC%20question">&#121;&#109;&#099;&#115;&#121;&#110;&#111;&#100;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;</a></div>
+				</div>
       </div>
 		)
 	}
